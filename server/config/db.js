42c69964aff388ba5620370d4128ce94e8a8e671
Henil2472 <113207001+Henil2472@users.noreply.github.com>
2022-10-27 20:47:37 -0400

    1

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // URI: "mongodb://127.0.0.1/car_store",
+  URI: "mongodb+srv://henilpatel:henil123@myportfolio.vydcqoo.mongodb.net/car_list?retryWrites=true&w=majority",
+  Secret: "SomeSecret",
+};
